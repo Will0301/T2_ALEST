000be2e5485ae8b41dc89c3bf43cc051edb790d3
Willian Ferreira Weyh <willian.weyh@edu.pucrs.br>
2023-11-13 20:11:17 -0300

    setup project

---
+++ entities/Elements.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Elements {
+
+    private String hydrogenUnits; //14
+    private String elementName; //cobre
+
+    private Integer elementQuant; //3
+
+    private Long totalHydrogen; // elementQuant * hydrogenUnits
+}
---
+++ service/filereader/ReaderService.java
@@ -0,0 +1,4 @@
+package service.filereader;
+
+public class ReaderService {
+}
